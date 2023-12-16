49bf6865faf63580848814cf445fb82b16bcf942
Aditya <adityawasudeo@gmail.com>
2023-12-16 14:52:28 +0000

    First lesson

--- lesson_00/src/app/home/home.component.ts
+++ lesson_00/src/app/home/home.component.ts
@@ -42,8 +42,12 @@ export class HomeComponent {
 
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-
+  filteredLocationList: HousingLocation[] = [];
+  
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 }
